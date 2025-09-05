20961a0a0621248692dbf644aee0fa2a9db755cf
shaotz <66196038+shaotz@users.noreply.github.com>
2025-09-05 10:01:12 +0800

    fixed many errors. now prints vendor and name

--- query_cpuid.asm
+++ query_cpuid.asm
@@ -1,14 +1,11 @@
 ; query_cpuid.asm
 
-extern print
-
 section .data
 	fmt_plain db "%s",0
-	str1 db 'Unable to set ID flag at 21h',' cpuid is not available', 10, 0
+	str1 db 'Unable to set ID flag at EFLAGS bit 21, cpuid is not available', 10, 0
 	len_str1 equ $-str1
 	
 	efmask dd 1 << 21
-	counter dd 0x80000002
 section .bss
 	cpuid_vendor resb 13
 	cpuid_cpuname resb 49
@@ -22,25 +19,27 @@ print:
 
 	mov eax, 4
 	mov ebx, 1	
-	mov edx, [esp+8]
-	mov ecx, [esp+12]
+	mov edx, [ebp+8]
+	mov ecx, [ebp+12]
 	int 0x80
 	
 	mov esp, ebp
 	pop ebp
 	ret
+
 checkCpuid:
 	pushfd
 	pop eax
 	pushfd
-	xor dword [esp], efmask	
+	mov ebx, [efmask]
+	xor [esp], ebx	
 	popfd
 	pushfd
-	and eax, dword [esp]
-	and eax, efmask
+	xor eax, dword [esp]
+	xor eax, ebx
 	cmp eax, 0
 	mov eax, 0x1
-	jne .end			; ef not changed
+	jne .end				; ef not changed
 	mov eax, 0x0
 .end:
 	popfd
@@ -74,7 +73,7 @@ _start:
 	lea edi, [cpuid_cpuname]
 	
 	xor esi, esi
-	mov esi, 0x80000002 ; using esi to keep leaf counter
+	mov esi, 0x80000002 			; using esi to keep leaf counter
 .getNameStrLoop:
 	mov eax, esi
 	xor ecx, ecx
